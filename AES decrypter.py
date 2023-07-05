e912f1af607c85ff4dec6387fe032002bdc7ca5d
tlamen <bernardolobo3@gmail.com>
2023-07-05 10:07:09 -0300

    InvShiftRows done

--- AES decrypter.py
+++ AES decrypter.py
@@ -160,10 +160,10 @@ def InvSubBytes(matrix):
 def InvShiftRows(matrix):
     new = []
 
-    new.append([matrix[0][0], matrix[1][1], matrix[2][2], matrix[3][3]])
-    new.append([matrix[1][0], matrix[2][1], matrix[3][2], matrix[0][3]])
-    new.append([matrix[2][0], matrix[3][1], matrix[0][2], matrix[1][3]])
-    new.append([matrix[3][0], matrix[0][1], matrix[1][2], matrix[2][3]])
+    new.append([matrix[0][0], matrix[3][1], matrix[2][2], matrix[1][3]])
+    new.append([matrix[1][0], matrix[0][1], matrix[3][2], matrix[2][3]])
+    new.append([matrix[2][0], matrix[1][1], matrix[0][2], matrix[3][3]])
+    new.append([matrix[3][0], matrix[2][1], matrix[1][2], matrix[0][3]])
 
     return new
 
