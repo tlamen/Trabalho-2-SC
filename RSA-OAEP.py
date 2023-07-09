72f69ac25078cce9df8bcfb35d42e5789710aa50
iIiqwertyiIi <nicpabe@gmail.com>
2023-07-09 19:04:01 -0300

    ajeitando um pouquinho

--- RSA-OAEP.py
+++ RSA-OAEP.py
@@ -1,148 +1,89 @@
 import os
 import sys
-# Python3 program Miller-Rabin primality test
 import random
 import math
 import numpy
 from SHA1hash import sha1
 from MGF1mask import mgf1
- 
-# Utility function to do
-# modular exponentiation.
-# It returns (x^y) % p
+
 def power(x, y, p):
-     
-    # Initialize result
-    res = 1;
-     
-    # Update x if it is more than or
-    # equal to p
-    x = x % p;
+    
+    res = 1
+    
+    x = x % p
     while (y > 0):
-         
-        # If y is odd, multiply
-        # x with result
         if (y & 1):
-            res = (res * x) % p;
- 
-        # y must be even now
-        y = y>>1; # y = y/2
-        x = (x * x) % p;
-     
-    return res;
- 
-# This function is called
-# for all k trials. It returns
-# false if n is composite and
-# returns false if n is
-# probably prime. d is an odd
-# number such that d*2<sup>r</sup> = n-1
-# for some r >= 1
+            res = (res * x) % p
+        y = y>>1
+        x = (x * x) % p
+    
+    return res
+
 def miillerTest(d, n):
-     
-    # Pick a random number in [2..n-2]
-    # Corner cases make sure that n > 4
-    a = 2 + random.randint(1, n - 4);
- 
-    # Compute a^d % n
-    x = power(a, d, n);
- 
+    a = 2 + random.randint(1, n - 4)
+
+    x = power(a, d, n)
+
     if (x == 1 or x == n - 1):
-        return True;
- 
-    # Keep squaring x while one
-    # of the following doesn't
-    # happen
-    # (i) d does not reach n-1
-    # (ii) (x^2) % n is not 1
-    # (iii) (x^2) % n is not n-1
+        return True
+
     while (d != n - 1):
-        x = (x * x) % n;
-        d *= 2;
- 
+        x = (x * x) % n
+        d *= 2
+
         if (x == 1):
-            return False;
+            return False
         if (x == n - 1):
-            return True;
- 
-    # Return composite
-    return False;
- 
-# It returns false if n is
-# composite and returns true if n
-# is probably prime. k is an
-# input parameter that determines
-# accuracy level. Higher value of
-# k indicates more accuracy.
+            return True
+
+    return False
+
 def isPrime(n, k):
-     
-    # Corner cases
+    
     if (n <= 1 or n == 4):
-        return False;
+        return False
     if (n <= 3):
-        return True;
- 
-    # Find r such that n =
-    # 2^d * r + 1 for some r >= 1
-    d = n - 1;
+        return True
+    
+    d = n - 1
     while (d % 2 == 0):
-        d //= 2;
- 
-    # Iterate given number of 'k' times
+        d //= 2
+
     for i in range(k):
         if (miillerTest(d, n) == False):
-            return False;
- 
-    return True;
- 
+            return False
 
-# Python3 program to find multiplicative modulo
-# inverse using Extended Euclid algorithm.
- 
-# Global Variables
+    return True
+
 x, y = 0, 1
- 
-# Function for extended Euclidean Algorithm
- 
- 
+
 def gcdExtended(a, b):
     global x, y
- 
-    # Base Case
+
     if (a == 0):
         x = 0
         y = 1
         return b
- 
-    # To store results of recursive call
+
     gcd = gcdExtended(b % a, a)
     x1 = x
     y1 = y
- 
-    # Update x and y using results of recursive
-    # call
+
     x = y1 - (b // a) * x1
     y = x1
- 
+
     return gcd
  
  
 def modInverse(A, M):
- 
+
     g = gcdExtended(A, M)
     if (g != 1):
         return
- 
+
     else:
- 
-        # m is added to handle negative x
         res = (x % M + M) % M
         return res
-
-# Driver Code
-# Number of iterations
- 
-# This code is contributed by mits
 
 not_possible = []
 k = 1000
